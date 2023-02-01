b0f603f847d812e2588eb5b983f87b50221346da
answerw3i <answerw3i@gmail.com>
2023-02-02 01:12:59 +0800

    1

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
 	fmt.Print("test")
+}
+
+func tcpScan(ip string) {
+	conn, err := net.Dial("tcp", ip)
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+	fmt.Print("Connect Sucessful")
+	conn.Close()
 }
